2e6c9f1159f3ff5862aeac37d18c6df81717d65a
Frank Greguska <89428916+frankinspace@users.noreply.github.com>
2023-09-13 16:03:36 -0700

    Add bucket policy (#38)
    * Add the required bucket policy to allow read access from cloudfront.
    
    * Update CF distribution id to be sensitive and try using environment for deployment.
    
    * missing get version step
    
    * missing get version step
    
    * switch to get npm version action
    
    * switch to get npm version action
    
    * /deploy sit
    
    * /deploy sit
    
    * /deploy sit
    
    * /deploy sit
    
    * /deploy sit
    
    * /deploy sit
    
    * /deploy sit
    
    * /deploy sit
    
    * /deploy sit
    
    * /deploy sit
    
    * /deploy sit
    
    * /deploy sit
    
    * /deploy sit
    
    ---------
    
    Co-authored-by: Frank Greguska <Francis.Greguska@jpl.nasa.gov>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,6 +2,16 @@ variable "stage" {}
 variable "credentials" {}
 variable "profile" {}
 variable "app_version" {}
+variable "cloudfront_distribution_id" {
+  type = string
+  description = "ID of the Cloudfront distribution routing public traffic to this app"
+  sensitive   = true
+}
+variable "cloudfront_allow_vpcs" {
+  type = string
+  description = "List of VPC ids which will be given read access to S3 bucket. Primarily for use if cloudfront traffic is coming from a different VPC."
+# Cannot be marked sensitive because it is used in a for_each https://github.com/hashicorp/terraform/issues/29744
+}
 variable "region" {
   default = "us-west-2"
 }
