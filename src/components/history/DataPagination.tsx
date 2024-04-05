9e77a60b9e7b57437a02228565720a4fa9550fd2
jbyrne <jimmy.byrne@jpl.nasa.gov>
2024-04-05 09:09:39 -0700

    feature/swodlr-ui-71: make end pagination faster

--- src/components/history/DataPagination.tsx
+++ src/components/history/DataPagination.tsx
@@ -2,23 +2,20 @@ import { Button, ButtonGroup, Pagination } from "react-bootstrap";
 import { Product } from "../../types/graphqlTypes";
 import { getUserProducts } from "../../user/userData";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import { setHistoryPageStateNext, setHistoryPageStatePrevious, setHistoryPageStateStart, setUserProducts } from "../sidebar/actions/productSlice";
+import { setHistoryPageState, setHistoryPageStateNext, setHistoryPageStatePrevious, setHistoryPageStateStart, setUserProducts } from "../sidebar/actions/productSlice";
 import { productsPerPage } from "../../constants/rasterParameterConstants";
 import { useState } from "react";
 import { ChevronBarLeft, ChevronBarRight, ChevronCompactLeft, ChevronCompactRight } from "react-bootstrap-icons";
 
 const DataPagination = () => {
     const dispatch = useAppDispatch()
-    // const currentDataPageId = useAppSelector((state) => state.product.currentDataPageId)
-    // const previousDataPageId = useAppSelector((state) => state.product.previousDataPageId)
-    // const twoBackDataPageId = useAppSelector((state) => state.product.twoBackDataPageId)
-    // const firstProducts = useAppSelector((state) => state.product.firstProducts)
     const historyPageState = useAppSelector((state) => state.product.historyPageState)
     const historyPageIndex = useAppSelector((state) => state.product.historyPageIndex)
     const firstHistoryPageData = useAppSelector((state) => state.product.firstHistoryPageData)
     const userProducts = useAppSelector((state) => state.product.userProducts)
     const [noNextPage, setNoNextPage] = useState<boolean>(false)
     const [noPreviousPage, setNoPreviousPage] = useState<boolean>(true)
+    const [tableLoading, setTableLoading] = useState<boolean>(false)
 
     const handleStart = () => {
         if(noNextPage) setNoNextPage(false)
@@ -34,11 +31,8 @@ const DataPagination = () => {
             dispatch(setHistoryPageStatePrevious())
             setNoPreviousPage(true)
         } else {
-            console.log('historyPageState: ',historyPageState)
-            console.log('historyPageIndex: ',historyPageIndex)
             await getUserProducts({limit: productsPerPage, after: historyPageState[historyPageIndex-3]}).then(response => {
                 const currentPageProducts = response.products as Product[]
-                console.log('response: ',response)
                 dispatch(setUserProducts(currentPageProducts))
                 dispatch(setHistoryPageStatePrevious())
             })
@@ -60,6 +54,7 @@ const DataPagination = () => {
     }
     
     const handleEnd = async (localPageState: string[], localPageIndex: number) => {
+        dispatch(setHistoryPageState(localPageIndex))
         const localPageStateToUse = [...localPageState]
         await getUserProducts({limit: productsPerPage, after: localPageStateToUse[localPageIndex-1]}).then(response => {
             const currentPageProducts = response.products as Product[]
@@ -87,7 +82,7 @@ const DataPagination = () => {
                     <Button style={{border: '#FAF9F6 solid 1px'}} onClick={() => handleStart()} disabled={noPreviousPage}><ChevronBarLeft color="white" size={18}/>Start</Button>
                     <Button style={{border: '#FAF9F6 solid 1px'}} onClick={() => handlePrevious()} disabled={noPreviousPage}><ChevronCompactLeft color="white" size={18}/>Previous</Button>
                     <Button style={{border: '#FAF9F6 solid 1px'}} onClick={() => handleNext()} disabled={userProducts.length < parseInt(productsPerPage) || noNextPage}>Next<ChevronCompactRight color="white" size={18}/></Button>
-                    <Button style={{border: '#FAF9F6 solid 1px'}} onClick={() => handleEnd(historyPageState, historyPageIndex)} disabled={userProducts.length < parseInt(productsPerPage) || noNextPage}>End<ChevronBarRight color="white" size={18}/></Button>
+                    <Button style={{border: '#FAF9F6 solid 1px'}} onClick={() => handleEnd(historyPageState, historyPageState.length-1)} disabled={userProducts.length < parseInt(productsPerPage) || noNextPage}>End<ChevronBarRight color="white" size={18}/></Button>
                 </ButtonGroup>
             </Pagination>
         </div>
