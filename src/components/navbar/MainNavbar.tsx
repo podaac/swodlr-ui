1993b8b1430787ab82a9cfeea6f49065a1ab2a8b
Jimmy <jbyrne5094@gmail.com>
2024-05-30 13:28:44 -0700

    Feature/swodlr UI 70 - add filtering to my data page and re-generation button (#105)
    * /version v1.0.0-rc.1
    
    * /version v1.0.0-rc.2
    
    * Pull in recent changes to 1.0.0
    
    * /version v1.0.0-rc.3
    
    * Fix umm-t record
    
    * /version v1.0.0-rc.4
    
    * Fix umm-t record
    
    * /version v1.0.0-rc.5
    
    * Fix umm-t record
    
    * /version v1.0.0-rc.6
    
    * /version v1.0.0-rc.7
    
    * issue/manual-granule-input-hotfix: fixed how ranged of scenes are processed (#86)
    
    Co-authored-by: jbyrne <jimmy.byrne@jpl.nasa.gov>
    
    * Issues/swodlr UI 72 essential UI bug fixes (#87)
    
    * issues/swodlr-ui-72: fix cps url params bug
    
    * issues/swodlr-ui-72
    
    * issues/swodlr-ui-75: fixed a couple bugs
    
    * issues/swodlr-ui-72: fixed map movement, adjust options, data page limit, etc
    
    * issues/swodlr-ui-72: changed spatial search beginning date in range
    
    * issues/swodlr-ui-72-essential-fixes: cleaned up comments
    
    ---------
    
    Co-authored-by: jbyrne <jimmy.byrne@jpl.nasa.gov>
    
    * /version v1.0.0-rc.8
    
    * feature/swodlr-ui-70: add my data filters
    
    * feature/swodlr-ui-70: fix re-generation button
    
    * feature/swodlr-ui-70: changed action buttons
    
    * feature/swodlr-ui-70: changed filter css
    
    * feature/swodlr-ui-70: add cycle pass scene input validation
    
    * feature/swodlr-ui-70: fix href for nav links
    
    * feature/swodlr-ui-70:
    
    ---------
    
    Co-authored-by: frankinspace <frankinspace@users.noreply.github.com>
    Co-authored-by: Frank Greguska <Francis.Greguska@jpl.nasa.gov>
    Co-authored-by: Jonathan M Smolenski <smolensk@jpl.nasa.gov>
    Co-authored-by: jonathansmolenski <jonathansmolenski@users.noreply.github.com>
    Co-authored-by: jbyrne <jimmy.byrne@jpl.nasa.gov>

--- src/components/navbar/MainNavbar.tsx
+++ src/components/navbar/MainNavbar.tsx
@@ -13,6 +13,7 @@ const MainNavbar = () => {
   const colorModeClass = useAppSelector((state) => state.navbar.colorModeClass)
   const userData = useAppSelector((state) => state.app.currentUser)
   const navigate = useNavigate()
+  const BASE_REDIRECT_URI = process.env.REACT_APP_BASE_REDIRECT_URI;
 
   var { email, firstName, lastName } = userData || {};
   const { search } = useLocation();
@@ -34,13 +35,13 @@ const MainNavbar = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link className={`${colorModeClass}-navbar-link`} onClick={() => navigate(`/customizeProduct/selectScenes${search}`)} id='customization-tab'>Customization</Nav.Link>
-          <Nav.Link className={`${colorModeClass}-navbar-link`} onClick={() => navigate(`/generatedProductHistory${search}`)} id='my-data-page'>My Data</Nav.Link>
+          <Nav.Link className={`${colorModeClass}-navbar-link`} href={`${BASE_REDIRECT_URI}customizeProduct/selectScenes${search}`} onClick={() => navigate(`/customizeProduct/selectScenes${search}`)} id='customization-tab'>Customization</Nav.Link>
+          <Nav.Link className={`${colorModeClass}-navbar-link`} href={`${BASE_REDIRECT_URI}generatedProductHistory${search}`} onClick={() => navigate(`/generatedProductHistory${search}`)} id='my-data-page'>My Data</Nav.Link>
           <NavDropdown  className={`${colorModeClass}-navbar-dropdown`} title="Data Discovery" id="basic-nav-dropdown" menuVariant="dark">
             <NavDropdown.Item href="https://search.earthdata.nasa.gov/search/granules?p=C2799438271-POCLOUD&pg[0][v]=f&pg[0][gsk]=-start_date&q=SWOT_L2_HR_RASTER_2.0&tl=1705536407!3!!&lat=65.390625&zoom=1" target="_blank">Earthdata Search</NavDropdown.Item>
             <NavDropdown.Item href="https://podaac.jpl.nasa.gov/" target="_blank">PO.DAAC Portal</NavDropdown.Item>
           </NavDropdown>
-          <Nav.Link className={`${colorModeClass}-navbar-link`} onClick={() => navigate(`/about${search}`)}>About</Nav.Link>
+          <Nav.Link className={`${colorModeClass}-navbar-link`} href={`${BASE_REDIRECT_URI}about${search}`} onClick={() => navigate(`/about${search}`)}>About</Nav.Link>
           <Nav.Link className={`${colorModeClass}-navbar-link`} onClick={() => dispatch(setShowTutorialModalTrue())} id='tutorial-page'>Tutorial</Nav.Link>
         </Nav>
         <NavDropdown  className={`${colorModeClass}-text`} title={renderUserDropdownTitle()} id="basic-nav-dropdown" align="end" style={{marginRight: '20px'}} menuVariant="dark">
